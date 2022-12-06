0ff00dcf74670a587537d8548c93fec224672df2
Florian Parzer <florian-parzer@gmx.at>
2022-12-06 19:53:31 +0100

    Inital Setup with SpringBoot created #1

---
+++ src/main/java/fhtw/LengthCalc/LengthCalcApplication.java
@@ -0,0 +1,13 @@
+package fhtw.LengthCalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LengthCalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LengthCalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fhtw/LengthCalc/LengthCalcApplicationTests.java
@@ -0,0 +1,13 @@
+package fhtw.LengthCalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LengthCalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
