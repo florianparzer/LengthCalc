89a1c468f961e58acd99582a337cf91e7dbf6b88
Florian Parzer <florian-parzer@gmx.at>
2022-12-06 20:34:54 +0100

    Added Test Cases #3

--- src/test/java/fhtw/LengthCalc/service/LengthCalcServiceTest.java
+++ src/test/java/fhtw/LengthCalc/service/LengthCalcServiceTest.java
@@ -10,5 +10,8 @@ class LengthCalcServiceTest {
     void getCentimeter() {
         LengthCalcService service = new LengthCalcService();
         assertEquals((float) 2.54, service.getCentimeter(1));
+        assertEquals((float) 12.7, service.getCentimeter(5));
+        assertEquals((float) 3.81, service.getCentimeter((float) 1.5));
+        assertEquals((float) 0, service.getCentimeter(0));
     }
 }
